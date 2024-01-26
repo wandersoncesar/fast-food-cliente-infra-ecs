d2cdbd08b678a904eaf31b6f97db0631d9675e47
Wanderson César <contato@wandersoncesar.com.br>
2024-01-26 14:37:00 -0300

    Update production.tf

--- production.tf
+++ production.tf
@@ -3,7 +3,7 @@ Variables used across all modules
 ======*/
 locals {
   production_availability_zones = ["us-east-1a", "us-east-1b"]
-  environment                   = "ff-cliente"
+  environment                   = "api-cliente"
 }
 
 provider "aws" {
@@ -13,15 +13,15 @@ provider "aws" {
 module "ecs" {
   source             = "./ecs"
   environment        = local.environment
-  vpc_id             = "vpc-0b36947dc0287234c"
+  vpc_id             = "vpc-03599603e61e08c59"
   availability_zones = local.production_availability_zones
   repository_name    = "fast-food-app"
-  subnets_ids        = ["subnet-02de05750f4eef167", "subnet-0ad9cbbc1f738f784"]
-  public_subnet_ids  = ["subnet-052bca6bb5af799b5", "subnet-0975413e62f9ae778"]
+  subnets_ids        = ["subnet-06ecf9a08b3b0f332", "subnet-0a828c9554d1127c0"]
+  public_subnet_ids  = ["subnet-0ed872fb39094e628", "subnet-055ecb6a3ba4588f9"]
   security_groups_ids = [
-    "sg-08d49485c0c3d3bef", "sg-045515a8381388922"
+    "sg-0a0e28979fbac271e", "sg-086fed6229b34bf5a"
   ]
-  database_endpoint = "fast-food-cliente-database.coojyhb5idp7.us-east-1.rds.amazonaws.com"
+  database_endpoint = "api-cliente-database.coojyhb5idp7.us-east-1.rds.amazonaws.com"
   database_name     = var.production_database_name
   database_username = var.production_database_username
   database_password = var.production_database_password
